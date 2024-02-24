f94fe8a5cb5fd8aa65f3b8181761322b1d211a3c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2024-02-24 23:05:48 +0200

    Create Logo.vue

---
+++ src/shared/components/UIComponents/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo-wrapper">
+    <a href="/" class="logo" :style="{ fontSize: fontSize + 'rem' }">VAL</a>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Logo',
+  props: {
+    fontSize: {
+      type: String,
+      default: '2'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/scss/variables.scss';
+.logo-wrapper {
+  text-align: center;
+  min-width: 100px;
+  padding-top: 1rem;
+  margin: 0;
+}
+.logo {
+  font-family: $raleway;
+  font-weight: 900;
+  font-size: 2rem;
+  color: $primary-main;
+
+  text-decoration: none;
+
+  &:hover {
+    color: $primary-main-hover;
+    transition: color $timeDelay $cubic;
+  }
+}
+</style>
